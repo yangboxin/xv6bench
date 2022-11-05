368cf8803d6e63785ec0accbfc07eaa9709b5fa5
yangboxin <1105270243@qq.com>
2022-11-05 11:19:41 +0800

    pipe version 0.1

---
+++ xv6-riscv-modified/user/pipetest.c
@@ -0,0 +1,52 @@
+#include "kernel/param.h"
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "kernel/riscv.h"
+#include "user/user.h"
+
+
+/* global variables */
+int	iter,flag;
+
+/* function that return result */
+void reporter()
+{
+ printf("%d\n", iter);
+ flag = 1;
+ sigreturn();
+}
+
+/* main test function */
+int main(int argc, char * argv[])
+{
+ int	duration;
+ char	buf[512];
+ int	pvec[2];
+ if (argc != 2)
+ {
+  printf("Usage: %s duration\n", argv[0]);
+  exit(1);
+ }	
+ duration = atoi(argv[1]); /* takes execution time from program argv params */
+ pipe(pvec);
+ flag = 0;
+ sigalarm(duration, reporter); /* end test execution in <duration> minutes and betrays management to <report> function*/
+ iter = 0;
+ while (flag != 1) /* until report is not executed do */
+   {
+    if (write(pvec[1], buf, sizeof(buf)) != sizeof(buf))
+      {
+       //printf("write file error\n");
+       break;
+      }
+    if (read(pvec[0], buf, sizeof(buf)) != sizeof(buf))
+      {
+       //printf("read file error\n");
+       break;
+      }
+    iter++; /* increase number of test cycles */
+   }
+   sigalarm(0,0);
+ return(0);
+}
+
